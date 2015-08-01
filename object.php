43e5b8291bf3c90c2d3b5b98e755cbeaeceed36b
Paul Bokel <paul.bokel@gmail.com>
2015-08-01 20:37:00 +0100

    Lesson 10  in between

--- object.php
+++ object.php
@@ -1,4 +1,11 @@
 <?php
+$object = array(
+	'title' => '',
+	'question' => '',
+	'description' => '',
+	'code' => '',
+	'date' => '',
+	 );
 $id = $_REQUEST['id'];
 $title='';
 $question = '';
@@ -7,18 +14,23 @@ $code = '';
 $date = '';
 
 if ($id ==1){
-	$title= 'Logging mentoring';
-	$question= 'Mentoring pairs';
-	$description = 'The mentor and mentee that have been paired';
-	$code = 'alert(This is an alert)';
-	$date = 'August 1, 2015';
+	$object = array(
+		'title' => 'Logging mentoring',
+		'question' => 'Mentoring pairs',
+		'description' => 'The mentor and mentee that have been paired',
+		'code' => 'alert(This is an alert)',
+		'date' => 'August 1, 2015',
+		);
 }
 elseif ($id ==2) {
-	$title= 'Logging mentoring';
-	$question= 'Mentoring pairs';
-	$description = 'Another question';
-	$code = 'ibase_blob_add(blob_handle, data)';
-	$date = 'August 31, 2016';
+	$object = array(
+		'title' => 'Logging mentoring',
+		'question' => 'Mentoring pairs',
+		'description' => 'Another question',
+		'code' => 'ibase_blob_add(blob_handle, data)',
+		'date' => 'August 31, 2016',
+		);
+
 }
 
 ?>
