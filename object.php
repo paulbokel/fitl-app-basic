72f0fd20b4cdee1b9752dc3f4a8384b11655711a
Paul Bokel <paul.bokel@gmail.com>
2015-08-02 16:06:09 +0100

    Dynamic with arrows!?

--- object.php
+++ object.php
@@ -1,4 +1,6 @@
 <?php
+$id = $_REQUEST['id'];
+
 $object = array(
 	'title' => '',
 	'question' => '',
@@ -6,46 +8,56 @@ $object = array(
 	'code' => '',
 	'date' => '',
 	 );
-$id = $_REQUEST['id'];
-$title='';
-$question = '';
-$description = '';
-$code = '';
-$date = '';
 
-if ($id ==1){
-	$object = array(
-		'title' => 'Logging mentoring',
-		'question' => 'Mentoring pairs',
-		'description' => 'The mentor and mentee that have been paired',
-		'code' => 'alert(This is an alert)',
-		'date' => 'August 1, 2015',
-		);
-}
-elseif ($id ==2) {
-	$object = array(
-		'title' => 'Logging mentoring',
-		'question' => 'Mentoring pairs',
-		'description' => 'Another question',
-		'code' => 'ibase_blob_add(blob_handle, data)',
-		'date' => 'August 31, 2016',
-		);
+//Database connection credentials
+$servername = 'localhost';
+$username = 'homestead';
+$password = 'secret';
 
+//create a connection
+$connection = new mysqli($servername, $username, $password);
+
+//check for errors
+if ($connection->connect_error){
+	echo 'Connection failed: '. $connection->connect_error;
+	exit;
 }
+
+//otherwise, connection successfully
+echo "connected successfully";
+
+//connect to the database
+$connection->select_db('fitl');
+
+$sql = 'SELECT * FROM pairs WHERE id = '. $id;
+
+//Execute query
+$result = $connection->query($sql);
+
+//Get the object
+if ($result->num_rows >0){
+	$object = $result->fetch_assoc();
+	//echo '<pre>';
+	//print_r($object);
+	//print_r($result);
+	//print_r($connection);
+	//echo '/pre>';
+}
+
 
 ?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title><?php echo $title; ?></title>
+		<title><?php echo $object['id']; ?></title>
 	</head>
 	<body>
-		<h1><?php echo $question; ?></h1>
-		<p><?php echo $description; ?></p>
+		<h1><?php echo $object['studentId']; ?></h1>
+		<p><?php echo $object['mentorId']; ?></p>
 		<pre>
-			<?php echo $code; ?>
+			<?php echo $object['startDate']; ?>
 		</pre>
-		<p>Question date: <?php echo $date; ?></p>
+		<p>Question date: <?php echo $object['endDate']; ?></p>
 	</body>
 
 </html>
